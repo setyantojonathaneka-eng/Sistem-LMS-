f2ac239540aa7733e0ed6305fc8eded2ab3a6061
Mellisa Rizki <mellisarizkir@gmail.com>
2026-04-02 20:49:12 +0700

    initial commit - setup models

---
+++ app/Http/Controllers/AdminController/TesterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\AdminController;
+
+use App\Http\Controllers\Controller;
+use Illumminate\Http\Request;
+
+class TesterController extends Controller
+{
+    public function index()
+    {
+        return "LearnPath";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController\TesterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', [TesterController::class, 'index']);
