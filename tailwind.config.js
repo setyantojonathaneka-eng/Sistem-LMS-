ef95b22880304e414c59ea439eda9c65fce34629
Sondynaufal <sondynaufal06@gmail.com>
2026-04-26 02:13:15 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2C4A5E',
+        background: '#F0EAE4',
+        sidebarRight: '#78A2CC',
+        cardBlue: '#3A7DEC',
+        boxDark: '#5E68A8',
+        btnDark: '#5E68A8',
+        accentText: '#F7D070'
+      },
+      fontFamily: {
+        sans: ['Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif', 'Nunito', 'ui-sans-systeserif', 'm-ui'],
+        cursive: ['"Brush Script MT"', '"Comic Sans MS"', 'cursive']
+      }
+    }
+  }
+}
